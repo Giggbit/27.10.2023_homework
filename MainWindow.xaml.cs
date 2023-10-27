1ef6e178bf786c6c9cc5b83e1fd6ca2e893df172
Gigg <danya19981017@gmail.com>
2023-10-27 20:39:54 +0200

    Updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,19 +24,14 @@ namespace _27._09._2023_Команды
     {
         private bool isChanged = false;
 
+        private string userType = "none";
+        string a;
+
         public MainWindow() {
             InitializeComponent();
 
             CommandBinding binding;
-            binding = new CommandBinding(ApplicationCommands.New);
-            binding.Executed += NewCommand;
-            CommandBindings.Add(binding);
-
-            binding = new CommandBinding(ApplicationCommands.Open);
-            binding.Executed += OpenCommand;
-            CommandBindings.Add(binding);
-
-            binding = new CommandBinding (ApplicationCommands.Close);
+            binding = new CommandBinding(ApplicationCommands.Close);
             binding.Executed += CloseCommand;
             CommandBindings.Add(binding);
         }
@@ -45,14 +42,138 @@ namespace _27._09._2023_Команды
         }
 
         private void OpenCommand(object sender, ExecutedRoutedEventArgs e) {
-            
-            isChanged = false;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+
+            if (openFileDialog.ShowDialog() == true) {
+                string selectedFilePath = openFileDialog.FileName;
+                try {
+                    string fileContent = File.ReadAllText(selectedFilePath);
+                    textBox.Text = fileContent;
+                    isChanged = false;
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Ошибка при открытии файла: " + ex.Message);
+                }
+            }
+        }
+
+        private void SaveCommand(object sender, ExecutedRoutedEventArgs e) {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+
+            if (saveFileDialog.ShowDialog() == true) {
+                string filePath = saveFileDialog.FileName;
+                string contentToSave = textBox.Text;
+                try {
+                    File.WriteAllText(filePath, contentToSave);
+                    isChanged = false;
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
+                }
+            }
         }
 
         private void CloseCommand(object sender, ExecutedRoutedEventArgs e) { 
             this.Close();
         }
 
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e) {
+            isChanged = true;
+        }
 
+        private void Command_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = isChanged;
+        }
+
+        private void UpdateMenu() {
+            switch (userType) {
+                case "none":
+                    DisableAllMenuItems();
+                    break;
+
+                case "trial":
+                    EnableFileItems();
+                    DisableEditItems();
+                    break;
+
+                case "pro":
+                    EnableAllMenuItems();
+                    break;
+
+                case null:
+                    DisableAllMenuItems();
+                    break;
+            }
+        }
+
+        private void CheckKey() {
+            UpdateMenu();
+        }
+
+        private void EnableFileItems() {
+            newMenuItem.IsEnabled = true;
+            openMenuItem.IsEnabled = true;
+            saveMenuItem.IsEnabled = true;
+
+            CommandBinding binding;
+            binding = new CommandBinding(ApplicationCommands.New);
+            binding.Executed += NewCommand;
+            binding.CanExecute += Command_CanExecute;
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Open);
+            binding.Executed += OpenCommand;
+            binding.CanExecute += Command_CanExecute;
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Save);
+            binding.Executed += SaveCommand;
+            binding.CanExecute += Command_CanExecute;
+            CommandBindings.Add(binding);
+        }
+        private void DisableFileItems() {
+            newMenuItem.IsEnabled = false;
+            openMenuItem.IsEnabled = false;
+            saveMenuItem.IsEnabled = false;
+
+            CommandBindings.Clear();
+        }
+
+        private void EnableEditItems() {
+            cutMenuItem.IsEnabled = true;
+            copyMenuItem.IsEnabled = true;
+            pasteMenuItem.IsEnabled = true;
+        }
+        private void DisableEditItems() {
+            cutMenuItem.IsEnabled = false;
+            copyMenuItem.IsEnabled = false;
+            pasteMenuItem.IsEnabled = false;
+        }
+
+        private void EnableAllMenuItems() {
+            EnableFileItems();
+            EnableEditItems();
+        }
+        private void DisableAllMenuItems(){
+            DisableFileItems();
+            DisableEditItems();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e) {
+            NewWindow newWindow = new NewWindow();
+            newWindow.Owner = this; 
+            this.IsEnabled = false;
+
+            newWindow.Closed += (s, eventArgs) => {
+                this.IsEnabled = true;
+                if (newWindow.DialogResult == false) {
+                    userType = newWindow.KeyValue;
+                    CheckKey();
+                }
+            };
+            newWindow.ShowDialog();
+        }
     }
 }
